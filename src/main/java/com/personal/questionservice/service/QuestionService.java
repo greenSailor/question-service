f7b31b2c9fb30a74870194551f3d9757f8f13e98
greenSailor <dondetisk@gmail.com>
2023-09-25 09:02:18 -0500

    Initial commit

---
+++ src/main/java/com/personal/questionservice/service/QuestionService.java
@@ -0,0 +1,83 @@
+package com.personal.questionservice.service;
+
+import com.personal.questionservice.dao.QuestionDao;
+import com.personal.questionservice.model.Question;
+import com.personal.questionservice.model.QuestionWrapper;
+import com.personal.questionservice.model.Response;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category),HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        questionDao.save(question);
+        return new ResponseEntity<>("success",HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(String categoryName, Integer numberOfQuestions) {
+        List<Integer> questions =  questionDao.findRandomQuestionsByCategory(categoryName,numberOfQuestions);
+        return new ResponseEntity<>(questions,HttpStatus.OK);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(List<Integer> questionIds) {
+        List<QuestionWrapper> questionWrappers = new ArrayList<>();
+        List<Question> questions = new ArrayList<>();
+
+        for(Integer id:questionIds){
+            questions.add(questionDao.findById(id).get());
+        }
+
+        for(Question question : questions){
+            QuestionWrapper questionWrapper = new QuestionWrapper();
+            questionWrapper.setId(question.getId());
+            questionWrapper.setQuestionTitle(question.getQuestionTitle());
+            questionWrapper.setOption1(question.getOption1());
+            questionWrapper.setOption2(question.getOption2());
+            questionWrapper.setOption3(question.getOption3());
+            questionWrapper.setOption4(question.getOption4());
+        }
+
+        return new ResponseEntity<>(questionWrappers,HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> getScore(List<Response> responses) {
+        int right = 0;
+        for(Response response:responses){
+            Question question = questionDao.findById(response.getId()).get();
+            if (response.getResponse().equals(question.getRightAnswer())){
+                right++;
+            }
+        }
+        return new ResponseEntity<>(right,HttpStatus.OK);
+    }
+}
